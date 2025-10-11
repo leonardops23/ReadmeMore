389cd48bdaf97d83947dd56479d6f552175afecc
Leonardo <ivanpachecoleo23@gmail.com>
2025-10-11 14:35:45 -0400

    .

--- web/app/components/footer/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Footer } from './Footer'
-export { default as FooterLink } from './FooterLink'
-export { default as FooterSection } from './FooterSection'
-export { default as SocialIcon } from './SocialIcon'
--- web/app/components/navbar/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as NavbarLogo } from './NavbarLogo';
-export { default as NavbarLink } from './NavbarLink';
-export { default as NavbarButton } from './NavbarButton';
-export { default as DesktopNavigation } from './DesktopNavigation';
-export { default as MobileNavigation } from './MobileNavigation';
-export { default as MobileMenuButton } from './MobileMenuButton';
-export { navigationItems, navbarConfig } from './navigationConfig';
-export type { NavigationItem } from './navigationConfig';
